435470d721c422a6d9010b4349999a3f5dc62b73
Hutch07 <chutchinson@ssi-mi.com>
2026-05-11 13:47:24 -0400

    Model added Flights and Users

--- core/admin.py
+++ core/admin.py
@@ -2,23 +2,67 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .models import User
+from .models import User, Site, Flight, Geojson, Group
+
+
+class AdminOnlyMixin:
+    def has_module_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        return request.user.group.filter(name='Admin').exists()
+
+    def has_view_permission(self, request, obj=None):
+        return self.has_module_permission(request)
+
+    def has_add_permission(self, request):
+        return self.has_module_permission(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self.has_module_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_module_permission(request)
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(AdminOnlyMixin, BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'company')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'group')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'company', 'password1', 'password2', 'is_staff', 'is_superuser', 'groups'),
+            'fields': ('username', 'email', 'company', 'password1', 'password2', 'is_staff', 'is_superuser', 'groups', 'group'),
         }),
     )
     list_display = ('username', 'email', 'company', 'is_staff')
     search_fields = ('username', 'email', 'company')
     ordering = ('username',)
+
+
+@admin.register(Group)
+class GroupAdmin(AdminOnlyMixin, admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Site)
+class SiteAdmin(AdminOnlyMixin, admin.ModelAdmin):
+    list_display = ('name', 'longitude', 'latitude')
+    search_fields = ('name',)
+
+
+@admin.register(Flight)
+class FlightAdmin(AdminOnlyMixin, admin.ModelAdmin):
+    list_display = ('aws_url', 'date', 'pilot', 'site')
+    search_fields = ('aws_url', 'pilot')
+    list_filter = ('date', 'site')
+
+
+@admin.register(Geojson)
+class GeojsonAdmin(AdminOnlyMixin, admin.ModelAdmin):
+    list_display = ('name', 'url', 'site')
+    search_fields = ('name', 'url')
--- core/models.py
+++ core/models.py
@@ -2,9 +2,73 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Group(models.Model):
+    ADMIN = 'Admin'
+    PILOT = 'Pilot'
+    CUSTOMER = 'Customer'
+    GENERAL = 'General'
+    
+    GROUP_CHOICES = [
+        (ADMIN, 'Admin'),
+        (PILOT, 'Pilot'),
+        (CUSTOMER, 'Customer'),
+        (GENERAL, 'General'),
+    ]
+    
+    name = models.CharField('Name', max_length=20, choices=GROUP_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'group'
+        verbose_name_plural = 'groups'
+
+
 class User(AbstractUser):
     company = models.CharField('Company', max_length=255, blank=True)
+    group = models.ManyToManyField(Group, related_name='users', blank=True)
 
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
+
+
+class Site(models.Model):
+    name = models.CharField('Name', max_length=255, unique=True)
+    longitude = models.FloatField('Longitude')
+    latitude = models.FloatField('Latitude')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'site'
+        verbose_name_plural = 'sites'
+
+
+class Flight(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='flights')
+    aws_url = models.CharField('AWS URL', max_length=500, unique=True)
+    date = models.DateField('Date')
+    pilot = models.CharField('Pilot', max_length=32)
+
+    def __str__(self):
+        return f"Flight on {self.date} by {self.pilot}"
+
+    class Meta:
+        verbose_name = 'flight'
+        verbose_name_plural = 'flights'
+
+
+class Geojson(models.Model):
+    name = models.CharField('Name', max_length=255, unique=True)
+    url = models.TextField('URL', unique=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='geojsons')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'geojson'
+        verbose_name_plural = 'geojsons'
